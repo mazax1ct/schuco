818d3a06af0d5c04fcbf94328a012dfc868a07cf
mazax1ct <vova@giveandget.ru>
2018-09-10 14:40:40 +0300

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -6,23 +6,6 @@ $(document).ready(function() {
     });
   }, 100);
 
-  //изменение шапки при скролле
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > $('.header').height()) {
-      $('.header').addClass('scrolled');
-    } else {
-      $('.header').removeClass('scrolled');
-    }
-  });
-
-  //открытие/закрытие меню
-  $(".js-menu-opener").click(function() {
-    $(this).toggleClass("is-active");
-    $("body").toggleClass("overflow");
-    $(".header__menu").toggleClass("is-open");
-    return false;
-  });
-
   //слайдер профилей
   if ($('.js-profiles').length) {
     $('.js-profiles').slick({
@@ -104,20 +87,74 @@ $(document).ready(function() {
     }
   }
 
+  //слайдер отзывов
+  if ($('.js-reviews').length) {
+    $('.js-reviews').slick({
+      adaptiveHeight: true,
+      autoplay: false,
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      mobileFirst: true,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            prevArrow: '<button class="slick-arrow slick-arrow--prev" aria-label="Назад" type="button"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#arrow_left"/></svg></button>',
+            nextArrow: '<button class="slick-arrow slick-arrow--next" aria-label="Вперед" type="button"><svg class="slick-arrow__icon" aria-hidden="true"><use xlink:href="#arrow_right"/></svg></button>',
+            arrows: true,
+            dots: false
+          }
+        }
+      ]
+    });
+  }
+
+  //изменение шапки при скролле
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > $('.header').height()) {
+      $('.header').addClass('scrolled');
+    } else {
+      $('.header').removeClass('scrolled');
+    }
+  });
+
+  //скролл меню
+  $(".header__menu-list").on("click", "a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href');
+    var offset = $('.header__inner').outerHeight();
+		var top = $(id).offset().top;
+    var result = top - offset;
+    $('body,html').animate({
+      scrollTop: result
+    }, 500, function() {
+      $(".js-menu-opener").removeClass("is-active");
+      $("body").removeClass("overflow");
+      $(".header__menu").removeClass("is-open");
+      var offset = $('.header__inner').outerHeight();
+      var result = top - offset;
+      $('body,html').animate({
+        scrollTop: result
+      }, 0);
+    });
+	});
+
+  //открытие/закрытие меню
+  $(".js-menu-opener").click(function() {
+    $(this).toggleClass("is-active");
+    $("body").toggleClass("overflow");
+    $(".header__menu").toggleClass("is-open");
+    return false;
+  });
+
   //переключение профиля
   $(".calc__window-block-inner").click(function() {
     $(".calc__window-block-inner").removeClass("is-active");
     $(this).addClass("is-active");
     return false;
   });
-
-  $(".header__menu-list").on("click", "a", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href');
-    var offset = $('.header__inner').outerHeight();
-		var top = $(id).offset().top - offset;
-		$('body,html').animate({
-      scrollTop: top
-    }, 500);
-	});
 });
