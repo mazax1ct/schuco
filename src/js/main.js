2270dd7ffdd5ee028272e08ae717f478646cff38
mazax1ct <vova@giveandget.ru>
2020-04-28 14:14:07 +0300

    orange

--- src/js/main.js
+++ src/js/main.js
@@ -130,34 +130,9 @@ $(document).ready(function() {
     return false;
   });
 
-  //переключение профиля
-  $(".calc__window-block-inner").click(function() {
-    $(".calc__window-block-inner").removeClass("is-active");
-    $(this).addClass("is-active");
-    $('.js-calc__form-image').attr('src', $(this).attr('data-image'));
-    return false;
-  });
-
-  //открытие попапа
-  $("[data-fancybox='form-popup']").fancybox({
-    touch: false,
-    infobar: false,
-    toolbar: false,
-    smallBtn: false,
-    buttons: ["close"],
-    animationEffect: false,
-    arrows: false
-	});
-
   //закрытие попапа
   $('.js-popup-close').on('click', function() {
   	$.fancybox.close();
   	return false;
-  });
-
-  $('.js-calc-change').click(function() {
-    $('.calc-change').toggleClass("is-active");
-    $(".calc__block").toggleClass("is-active");
-    return false;
   });
 });
